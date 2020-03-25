d6d82ac5013b31b91fc7e9cd809e310d1b747472
Rupanshu verma <vermarupanshu733@gmail.com>
2020-03-25 13:54:37 +0530

    Darjiwala basic skeleton pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import img from './assets/image/background.jpg';
+import { Nav, Navbar, Container } from 'react-bootstrap';
+import About from './components/about/About';
+import Home from './components/home/Home';
+import Contact from './components/contact/Contact';
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+
+
+  dyHeight = () => {
+    return document.getElementById('navBar').clientHeight; 
+  }
+
+  render() {
+
+    return (
+      <>
+        <Container fluid className="nav-primary-text-dw">
+          <Navbar fixed="top" className="nav-primary-bg-dw" id="navBar">
+            <Nav>
+              <Nav.Item>
+                <AnchorLink offset={this.dyHeight} href='#home' className="primary-brand">Darjiwala.in</AnchorLink>
+              </Nav.Item>
+              <Nav.Item>
+                <AnchorLink offset={this.dyHeight} href='#about'>About</AnchorLink>
+              </Nav.Item>
+              <Nav.Item>
+                <AnchorLink offset={this.dyHeight} href='#contact'>Contact</AnchorLink>
+              </Nav.Item>
+              <Nav.Item>
+
+              </Nav.Item>
+            </Nav>
+          </Navbar>
+        </Container>
+        <Container fluid className="full-height">
+          <Home></Home>
+          <About></About>
+          <Contact></Contact>
+        </Container>
+      </>
+    );
+  }
 }
 
 export default App;
