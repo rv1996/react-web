f9a6951b50648a3f6011959c4fb447b91da02057
Rupanshu verma <vermarupanshu733@gmail.com>
2020-05-01 19:51:33 +0530

    Updates

---
+++ precache-manifest.694259e31605d2e3054555ee39c3daf6.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "400d160da33d23f29cad",
+    "url": "/react-web/static/css/main.3bcc43b7.chunk.css"
+  },
+  {
+    "revision": "400d160da33d23f29cad",
+    "url": "/react-web/static/js/main.400d160d.chunk.js"
+  },
+  {
+    "revision": "922de82dafacb957551f",
+    "url": "/react-web/static/js/1.922de82d.chunk.js"
+  },
+  {
+    "revision": "d7ab73ce6b5a3763b28c",
+    "url": "/react-web/static/js/runtime~main.d7ab73ce.js"
+  },
+  {
+    "revision": "a4b90faedf3164910ad3517b5e204960",
+    "url": "/react-web/static/media/illustration.a4b90fae.svg"
+  },
+  {
+    "revision": "a029dbda85529b0b2b2ac707d76470ff",
+    "url": "/react-web/static/media/Jackets.a029dbda.png"
+  },
+  {
+    "revision": "4be71d029405dc5b6776c8f61ed7cdb6",
+    "url": "/react-web/static/media/Jodhpuri.4be71d02.png"
+  },
+  {
+    "revision": "0f22dfdadaf99b18df678579fbb8fe9f",
+    "url": "/react-web/static/media/kurtaPyajama.0f22dfda.png"
+  },
+  {
+    "revision": "35c833cc011aa9fa19d7ced4ea3bb85b",
+    "url": "/react-web/static/media/Leisure.35c833cc.png"
+  },
+  {
+    "revision": "b8c24e5883d4facede2329a5aa701fdc",
+    "url": "/react-web/static/media/Sherwani.b8c24e58.png"
+  },
+  {
+    "revision": "e283df0b1b7f87eec8cdcda690c877d5",
+    "url": "/react-web/static/media/Rakesh_Maria_Natwar_Varma.e283df0b.png"
+  },
+  {
+    "revision": "825be7ae112682a43c0b25dfbdd72104",
+    "url": "/react-web/static/media/Raj_K-Purohit.825be7ae.jpg"
+  },
+  {
+    "revision": "52feee8d9c77a773bff272ad5f2bce3a",
+    "url": "/react-web/static/media/Vijay_Darda.52feee8d.jpg"
+  },
+  {
+    "revision": "0d30554ac15acbc1be7ddc85a3270be4",
+    "url": "/react-web/static/media/PC_Alexander.0d30554a.jpg"
+  },
+  {
+    "revision": "1b55fee684d61bfeaa762684931b1bc9",
+    "url": "/react-web/static/media/Karla-Regular.1b55fee6.ttf"
+  },
+  {
+    "revision": "5b4c93464c866fb842c39d0c1395b0ac",
+    "url": "/react-web/index.html"
+  }
+];
