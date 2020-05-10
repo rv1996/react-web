0ad8f5c19de6d0260749751345073e06c20f6cb5
Rupanshu Verma <vermarupanshu733@gmail.com>
2020-05-10 12:43:24 +0530

    Readme.md fixed, logo change, warninig fixed

--- src/App.js
+++ src/App.js
@@ -6,18 +6,17 @@ import Portfolio from './components/Portfolio';
 import Testimonials from  './components/Testimonials';
 import ContactUs from './components/ContactUs';
 import Footer from './components/Footer';
-import resumeData from './resumeData';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header resumeData={resumeData}/>
-        <About resumeData={resumeData}/>
-        <Resume resumeData={resumeData}/>
-        <Portfolio resumeData={resumeData}/>
-        <Testimonials resumeData={resumeData}/>
-        <ContactUs resumeData={resumeData}/>
-        <Footer resumeData={resumeData}/>
+        <Header />
+        <About />
+        <Resume />
+        <Portfolio />
+        <Testimonials />
+        <ContactUs />
+        <Footer />
       </div>
     );
   }
